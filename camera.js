d66beee7264448f59af1649a6de14b3de038e688
erhrehreh erhgerh <levendeevp@gmail.com>
2016-11-22 13:44:00 +0300

    100

---
+++ camera.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+public var target: Transform;
+
+function Start () {
+
+}
+
+function Update () {
+
+    transform.LookAt(target);
+
+    transform.position = target.position;
+
+    var pCam = GetComponent.<Transform>();
+pCam.position.z = pCam.position.z - 20;
+pCam.position.y = pCam.position.y + 5;
+
+
+}
---
+++ s1.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+
+function Start () {
+
+Debug.Log( 1 + 2 );
+
+}
+
+
+function Update () {
+
+var pSpeed = GetComponent.<Transform>();
+pSpeed.position.z = pSpeed.position.z + 0.5;
+
+
+
+var MoveLR = GetComponent.<Transform>();
+
+if ( Input.GetKey(KeyCode.D) ) {
+
+MoveLR.position.x = MoveLR.position.x + 0.1;
+
+}
+
+if (Input.GetKey(KeyCode.A)) {
+
+MoveLR.position.x = MoveLR.position.x - 0.1;
+
+}
+
+if (Input.GetKey(KeyCode.W)) {
+
+pSpeed.position.z = pSpeed.position.z + 0.5;
+
+}
+
+if (Input.GetKey(KeyCode.S)) {
+
+pSpeed.position.z = pSpeed.position.z - 0.2;
+
+}
+
+
+
+
+}
